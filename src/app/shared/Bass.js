f01f3a3d169ec2783766197fdbe43bc2a75ea985
PGA <paulgadam@gmail.com>
2023-01-20 02:43:38 -0700

    checked out GforG footer...not for me

--- src/app/shared/Bass.js
+++ src/app/shared/Bass.js
@@ -1,32 +1,27 @@
-import guitar5328Img from '../assets/img/p_region_RG5328_LDK_00_02.png'
 import bass1506Img from '../assets/img/p_region_EHB1506MS_BIF_1P_01.png';
 import bass5006Img from '../assets/img/p_region_SR5006_OL_00_05.png';
-import guitarImg from '../assets/img/p_region_RG8527Z_SDE_00_03.png';
-//WAS CAMPSITES
-export const GUITAR = [
+import bass1500Img from '../assets/img/p_region_EHB1500_DEF_1P_01.png';
+export const PARTNERS = [
     {
         id: 0,
-        name: 'Rg Guitars',
-        image: guitarImg,
-        elevation: 1233,
-        featured: true,
+        name: 'SR 5000',
+        image: bass5006Img,
+        featured: false,
         description:
-            'This 7 string monster, coming with a lo-pro edge (edge zero), Dimarzio PAF 7 pickups and a Macassar Ebony Fretboard'
+            'A standard 34" 6-string Tuned B-C with Barts and all the trimings'
     },
     {
         id: 1,
-        name: 'RG 5328',
-        image: guitar5328Img,
-        elevation: 877,
+        name: 'SR EBH 1500',
+        image: bass1500Img,
         featured: false,
         description:
-            'This 8 string monster, coming with a Gibralter II-8 bridge, Dimarzio Fusion Edge 8 pickups and a Macassar Ebony Fretboard'
+            'A multiscale 4-string Tuned B-G with Barts and all the trimings'
     },
     {
         id: 2,
         name: 'SR 5006',
         image: bass5006Img,
-        elevation: 2901,
         featured: false,
         description:
             'A standard 34" 6-string Tuned B-C with Barts and all the trimings'
@@ -35,7 +30,6 @@ export const GUITAR = [
         id: 3,
         name: 'SR 1506',
         image: bass1506Img,
-        elevation: 2901,
         featured: false,
         description:
             'A standard 34" 6-string Tuned B-C with Barts and all the trimings'
