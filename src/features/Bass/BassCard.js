4a56f5eec48c11b9eb25461c91c606ce4f99d729
PGA <paulgadam@gmail.com>
2023-01-20 23:08:21 -0700

    tried to debug null value issue in bass array...guess we keep it as a true value....

--- src/features/Bass/BassCard.js
+++ src/features/Bass/BassCard.js
@@ -2,17 +2,20 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const BassCard = ({ bass }) => {
-    const { id, image, name } = bass;
-    return (
-        <Link to={`${id}`}>
-            <Card>
-                <CardImg width='100%' src={image} alt={name} />
-                <CardImgOverlay>
-                    <CardTitle>{name}</CardTitle>
-                </CardImgOverlay>
-            </Card>
-        </Link>
-    );
+    if (bass) {
+        const { id, image, name } = bass;
+        return (
+            <Link to={`${id}`}>
+                <Card>
+                    <CardImg width='100%' src={image} alt={name} />
+                    <CardImgOverlay>
+                        <CardTitle>{name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
+            </Link>
+        );
+    };
+    return null;
 }
 
 
--- src/features/Bass/BassDetail.js
+++ src/features/Bass/BassDetail.js
@@ -1,18 +1,20 @@
 import { Card, CardImg, CardText, CardBody, Col } from 'reactstrap';
 
-const BassDetail = ( {bass} ) => {
-    const { id, image, name, description } = bass;
+const BassDetail = ({ bass }) => {
+    if (bass) {
+        const { id, image, name, description } = bass;
 
-    return (
-        <Col md='5' className='m-1'>
-            <Card>
-                <CardImg top src={image} alt={name} />
-                <CardBody>
-                    <CardText>{description}</CardText>
-                </CardBody>
-            </Card>
-        </Col>
-    );
+        return (
+            <Col md='5' className='m-1'>
+                <Card>
+                    <CardImg top src={image} alt={name} />
+                    <CardBody>
+                        <CardText>{description}</CardText>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    }; return null;
 };
 
 export default BassDetail;
