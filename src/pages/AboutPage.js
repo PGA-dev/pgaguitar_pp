0c04cf9a863953992551e4c20b4daf1f4392dd46
PGA <paulgadam@gmail.com>
2023-01-22 01:07:31 -0700

    updated some carp

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,25 +5,34 @@ import GuitarFlip from "../features/Flip/guitarFlip";
 const AboutPage = () => {
     return (
         <Container>
-            <SubHeader current={'Guitar Flip'} />
+            <SubHeader current={'About the Index'} />
             <Row>
                 <Col>
-                <br></br>
-                <br></br>
-
-
+                    <br></br>
+                    <br></br>
                 </Col>
             </Row>
             <Row className='row-content ms-auto'>
-
-                    <GuitarFlip />
-
-
-
+                <GuitarFlip />
             </Row>
-<Row>
-
-</Row>
+            <Row>
+                <Col>
+                    <br></br>
+                    <br></br>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <Card>
+                    <CardHeader className="bg-primary">
+                        Header for my card
+                    </CardHeader>
+                    <CardBody className="bg-danger">
+                        Body for my guitar card and such
+                    </CardBody>
+                </Card>
+                </Col>
+            </Row>
 
         </Container>
     );
