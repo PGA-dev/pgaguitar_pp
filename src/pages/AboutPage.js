315dd8288f281c9b1df998c2127abe9d960b7b50
PGA <paulgadam@gmail.com>
2023-01-22 00:50:05 -0700

    updated flip...fn works

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,6 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
 import SubHeader from "../components/SubHeader";
-
+import GuitarFlip from "../features/Flip/guitarFlip";
 
 const AboutPage = () => {
     return (
@@ -8,21 +8,19 @@ const AboutPage = () => {
             <SubHeader current={'Guitar Flip'} />
             <Row className='row-content'>
                 <Col sm='6'>
-
+<GuitarFlip />
                 </Col>
-                <Col sm='6'>
-                    <Card>
+
+                <Col >
+
+                              <Card>
                         <CardHeader className='bg-primary text-white'>
                             <h2>Flip</h2>
                         </CardHeader>
                         <CardBody>
-
+                            
                         </CardBody>
                     </Card>
-                </Col>
-                <Col >
-
-          
                 </Col>
             </Row>
 
