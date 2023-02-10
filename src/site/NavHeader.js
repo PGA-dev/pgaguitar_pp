5db452eca34382bb0b78c0e394f642c50b10c4a8
PGA <paulgadam@gmail.com>
2023-02-10 02:12:44 -0700

    updated css for gd

--- src/site/NavHeader.js
+++ src/site/NavHeader.js
@@ -17,6 +17,7 @@ import {
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import { useSpring, animated } from 'react-spring';
+import styles from './styleMod/Nav.module.css';
 
 const NavHeader = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -39,34 +40,33 @@ const NavHeader = () => {
     }, []);
 
     return (
-
         <Navbar id='navhead' dark color='primary' sticky='top' expand='lg'>
             <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
             <Collapse isOpen={menuOpen} navbar>
                 <Nav className='me-auto' navbar>
                     <animated.div style={animatedStyle2}>
                         <NavItem>
-                            <NavLink className='nav-link' to='/' style={{ color: "magenta" }}>
+                            <NavLink className='nav-link' to='/' style={navHead1}>
                                 <i style={{ color: "SlateBlue" }} className='fa fa-h-square fa-lg' /> Home
                             </NavLink>
                         </NavItem>
                     </animated.div>
                     <animated.div style={animatedStyle}>
                         <UncontrolledDropdown nav inNavbar >
-                            <DropdownToggle   nav caret style={{ color: "aqua" }}>
-                                <div style={{ color: "aqua" }} className='fa fa-cogs fa-sm'><span style={{ color: "magenta", fontFamily:'papyrus' }} >Guide Directory</span></div>
+                            <DropdownToggle   nav caret style={navHead2}>
+                                <div style={{ color: "aqua" }} className='fa fa-cogs fa-sm'><span style={navHead1} >Guide Directory</span></div>
                             </DropdownToggle>
                             <DropdownMenu right>
-                                <DropdownItem  style={{ color: "aqua", fontFamily:'papyrus' }}  href='/guidedirectory/0'>Guitar Body</DropdownItem>
-                                <DropdownItem style={{ color: "magenta", fontFamily:'papyrus' }} href='/guidedirectory/1'>Guitar Neck</DropdownItem>
-                                <DropdownItem style={{ color: "aqua", fontFamily:'papyrus' }} href='/guidedirectory/2'>Guitar Strings</DropdownItem>
-                                <DropdownItem style={{ color: "magenta", fontFamily:'papyrus' }} href='/guidedirectory/3'>Guitar Bridges</DropdownItem>
-                                <DropdownItem style={{ color: "aqua", fontFamily:'papyrus' }} href='/guidedirectory/4'>Guitar Pickups</DropdownItem>
-                                <DropdownItem style={{ color: "magenta", fontFamily:'papyrus' }} href='/guidedirectory/5'>Guitar Frets</DropdownItem>
-                                <DropdownItem style={{ color: "aqua", fontFamily:'papyrus' }} href='/guidedirectory/6'>Guitar Picks</DropdownItem>
-                                <DropdownItem style={{ color: "magenta", fontFamily:'papyrus' }} href='/guidedirectory/7'>Guitar Brands</DropdownItem>
-                                <DropdownItem style={{ fontFamily:'papyrus', border: 'aqua ridge 2px' }} divider />
-                                <DropdownItem style={{ color: "aqua", fontFamily:'papyrus' }} href='/guidedirectory'>Guide Directory</DropdownItem>
+                                <DropdownItem  style={navList2}  href='/guidedirectory/0'>Guitar Body</DropdownItem>
+                                <DropdownItem style={navList1} href='/guidedirectory/1'>Guitar Neck</DropdownItem>
+                                <DropdownItem style={navList2} href='/guidedirectory/2'>Guitar Strings</DropdownItem>
+                                <DropdownItem style={navList1} href='/guidedirectory/3'>Guitar Bridges</DropdownItem>
+                                <DropdownItem style={navList2} href='/guidedirectory/4'>Guitar Pickups</DropdownItem>
+                                <DropdownItem style={navList1} href='/guidedirectory/5'>Guitar Frets</DropdownItem>
+                                <DropdownItem style={navList2} href='/guidedirectory/6'>Guitar Picks</DropdownItem>
+                                <DropdownItem style={navList1} href='/guidedirectory/7'>Guitar Brands</DropdownItem>
+                                <DropdownItem style={navborder} divider />
+                                <DropdownItem style={navList2} href='/guidedirectory'>Guide Directory</DropdownItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
                     </animated.div>
@@ -92,9 +92,9 @@ const NavHeader = () => {
                         </NavItem>
                     </animated.div>
                     <animated.div style={animatedStyle}>
-                        <Form>
-                            <FormGroup>
-                                <Input
+                        <Form >
+                            <FormGroup >
+                                <Input className={styles.search_box}
                                     id="guideSearch"
                                     name="search"
                                     placeholder="Guide Search"
@@ -110,5 +110,43 @@ const NavHeader = () => {
 
     );
 };
+//(css alternative) stylesheet
+const navHead1= {
+    color: "magenta",
+    fontFamily: "papyrus"
+};
+
+const navHead2= {
+    color: "aqua",
+    fontFamily: "papyrus"
+};
+
+const navList1= {
+    color: "magenta",
+    padding: "15px",
+    margin: "5px 40px",
+    fontSize: "25px",
+    fontFamily: "papyrus"
+};
+
+const navList2= {
+    color: "aqua",
+    padding: "15px",
+    margin: "5px 40px",
+    fontSize: "25px",
+    fontFamily: "papyrus"
+};
+
+const navborder= {
+    color: "aqua",
+    border: "aqua ridge 2px",
+    fontFamily: "papyrus"
+};
+
+const searchBox= {
+    color: "aqua",
+    fontFamily: "papyrus"
+};
+
 
 export default NavHeader;
