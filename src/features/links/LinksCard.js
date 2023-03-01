74827beff7ebfd1840287379e89680f820ca676c
PGA <paulgadam@gmail.com>
2023-03-01 05:19:07 -0700

    fixed a link page bug

--- src/features/links/LinksCard.js
+++ src/features/links/LinksCard.js
@@ -1,9 +1,46 @@
 import { Card, CardText } from 'reactstrap';
 import { linksUrl } from '../../sitemisc/linksUrl';
+import { useState } from 'react';
 
 //Used in Site Map
 const LinksCard = ({ links }) => {
     const { id, name, url } = links;
+
+
+    const link = {
+        color: 'aqua',
+        padding: '15px',
+        margin: '20px',
+        fontSize: '35px',
+        fontFamily: 'papyrus',
+        fontWeight: '700',
+        textDecoration: 'none'
+    }
+    const linkHover = {
+        color: '#a424ff',
+        padding: '15px',
+        margin: '20px',
+        fontSize: '37px',
+        fontFamily: 'papyrus',
+        fontWeight: '900',
+        textDecoration: 'none'
+    }
+
+    const cardCSS = {
+        margin: '20px',
+        border: '3px solid slateblue',
+        borderRadius: '35px'
+    }
+
+    const useLink = () => {
+        const [style, setStyle] = useState(link);
+        const onMouseEnter = () => setStyle(linkHover)
+        const onClick = () => setStyle(linkHover)
+        const onMouseLeave = () => setStyle(link)
+        return { style, onClick, onMouseEnter, onMouseLeave }
+    }
+    const useL = useLink();
+
 
     const cardURL = linksUrl + url;
     return (
@@ -15,11 +52,6 @@ const LinksCard = ({ links }) => {
 }
 
 
-const cardCSS = {
-    margin: '20px',
-    border: '3px solid slateblue',
-    borderRadius: '35px'
-}
 
 
 export default LinksCard;
--- src/pages/GuideAboutPage.js
+++ src/pages/GuideAboutPage.js
@@ -20,7 +20,7 @@ const GuitarBodyPage = () => {
                     </div>
                 </Col>
             </Row>
-            <Row className='row-border align-items-center'>
+            <Row className='align-items-center'>
                 <Col sm='12'>
 
                 </Col>
