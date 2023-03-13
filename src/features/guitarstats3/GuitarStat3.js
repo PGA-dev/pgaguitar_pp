2f451999f05eae049297e52a769f81093c7fb8df
PGA-dev <paulgadam@gmail.com>
2023-03-13 00:53:11 -0600

    previd

--- src/features/guitarstats3/GuitarStat3.js
+++ src/features/guitarstats3/GuitarStat3.js
@@ -1,12 +1,15 @@
 
 
 const GuitarStat3 = ({ guitarstat3 }) => {
-    const {videoId, text } = guitarstat3;
+    const {videoId, text, videoId2, text2 } = guitarstat3;
     return (
         <>
             <ul className='list-unstyled' style={{ padding: '2px', fontFamily: 'papyrus', color: 'slateblue' }}>
                 <li>
                     <a href={videoId} target="_blank" rel="noreferrer">{text}</a>
+                </li>
+                <li>
+                    <a href={videoId2} target="_blank" rel="noreferrer">{text2}</a>
                 </li>
             </ul>
         </>
